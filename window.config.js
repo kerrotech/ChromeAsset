e3bb93d17c8080aca079b54461148e58bf4a7e8a
Kerrotech <Keagen.rs@gmail.com>
2026-04-28 07:34:43 -0400

    1.1.1

--- window.config.js
+++ window.config.js
@@ -1,4 +1,4 @@
 // Set this to your hosted kiosk web app URL.
 // GitHub Pages format:
 // "https://YOUR_GITHUB_USERNAME.github.io/YOUR_REPOSITORY_NAME/"
-window.KIOSK_WEB_APP_URL = "";
+window.KIOSK_WEB_APP_URL = "https://kerrotech.github.io/ChromeAsset/";
